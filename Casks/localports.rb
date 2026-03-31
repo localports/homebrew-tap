85f7a58adc50f05226041aeef2185cdcf077620d
jsgogodev-afk <jsgogo.dev@gmail.com>
2026-03-31 17:14:28 +0900

    fix: use homebrew-tap releases for DMG download (private repo fix)

--- Casks/localports.rb
+++ Casks/localports.rb
@@ -2,7 +2,7 @@ cask "localports" do
   version "1.0.0"
   sha256 "a7eb594b173ecad58aa1723cf7c75214a738636fbf6c188eacbd3309f6427a22"
 
-  url "https://github.com/localports/localports/releases/download/v#{version}/LocalPorts-#{version}.dmg"
+  url "https://github.com/localports/homebrew-tap/releases/download/v#{version}/LocalPorts-#{version}.dmg"
   name "LocalPorts"
   desc "macOS menu bar app for monitoring localhost servers"
   homepage "https://github.com/localports/localports"
